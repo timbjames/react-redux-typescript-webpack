1ef406f5a87fa695e2d8911076af7fbac520f90f
Tim James <tim@kingfisher-systems.co.uk>
2017-07-25 14:10:17 +0100

    removed generated files

--- SimpleTSTest/Scripts/Index.tsx
+++ SimpleTSTest/Scripts/Index.tsx
@@ -1,12 +1,43 @@
 ﻿import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class Index extends React.Component<{}, {}>{
 
     render() {
 
+        const Home = () => {
+            return <div><h1>Home</h1></div>
+        };
+
+        const One = () => {
+            return <div><h1>One</h1></div>
+        };
+
+        const Two = () => {
+            return <div><h1>Two</h1></div>
+        };
+
         return (
-            <h1>Here</h1>
+            <Router>
+
+                <div>
+
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/one">One</Link></li>
+                        <li><Link to="/two">Two</Link></li>
+                    </ul>
+
+                    <hr />
+
+                    <Route exact={ true } path="/" component={ Home } />
+                    <Route exact={ true } path="/one" component={ One } />
+                    <Route exact={ true } path="/two" component={ Two } />
+
+                </div>
+
+            </Router>
         );
     }
 }
