3f0e1b6b0ec2b4e2ffde64c9a982a1e9bf08bbce
Tim James <tim@kingfisher-systems.co.uk>
2017-07-25 20:49:52 +0100

    introduced redux successfully

--- SimpleTSTest/Scripts/Index.tsx
+++ SimpleTSTest/Scripts/Index.tsx
@@ -3,9 +3,111 @@ import * as ReactDOM from 'react-dom';
 import { RouteProps } from 'react-router';
 import { BrowserRouter as Router, Route, RouteComponentProps, Link } from 'react-router-dom';
 
+import { combineReducers } from 'redux';
+import { createAction, handleActions, Action } from 'redux-actions';
+
+import { ReduxContainerBuilder } from './ReduxContainer';
+
 interface IRouteParams extends RouteComponentProps<{ id: number }> { }
 
-class MyRoute extends Route<RouteProps> {}
+class MyRoute extends Route<RouteProps> { }
+
+interface IAreaState {
+    name: string;
+}
+
+interface IMyActions {
+    doSomething: () => void;
+    receive: (name: string) => void;
+}
+
+const initialState = (): IAreaState => {
+    return {
+        name: ''
+    };
+}
+
+class MyActions implements IMyActions {
+
+    public doSomething = () => (dispatch: any): void => {
+
+        dispatch(this.receive('tim'));
+    }
+
+    public receive = createAction('TEST', (name: string) => name);
+}
+
+const actionsDispatcherFactor = (dispatch: any): IMyActions => {
+
+    const localActions = new MyActions();
+
+    return {
+        doSomething: () => {
+            dispatch(localActions.doSomething());
+        },
+        receive: (name: string) => {
+            dispatch(localActions.receive(name));
+        }
+    }
+}
+
+const areaStateReducer = handleActions<IAreaState>({
+
+    'TEST': (state: IAreaState, action: Action<string>): IAreaState => {
+
+        return { name: action.payload };
+    }
+
+}, initialState());
+
+const rootReducer = combineReducers({
+    areaState: areaStateReducer
+});
+
+interface IReduxComponentProps extends React.Props<ReduxComponent> {
+    areaActions: IMyActions;
+    areaState: IAreaState;
+}
+
+class PageComponent extends React.Component<IReduxComponentProps, {}>{
+
+    render() {
+
+        const { areaActions, areaState } = this.props;
+
+        return (
+            <div>
+                <h1>State Object Yo {areaState.name}</h1>
+                <button onClick={() => {
+                    areaActions.doSomething();
+                }}>CLick Me</button>
+            </div>
+        );
+    }
+}
+
+class ReduxComponent extends React.Component<IReduxComponentProps, {}>{
+
+    render() {
+
+        const ReduxWrapper = () => {
+            return ReduxContainerBuilder().NoneProps({
+                mapDispatchToProps: (dispatch: any) => ({
+                    areaActions: actionsDispatcherFactor(dispatch)
+                }),
+                mapStateToProps: (state: any) => ({
+                    areaState: state.areaState
+                }),
+                pageComponent: PageComponent,
+                rootReducer: rootReducer
+            });
+        };
+
+        return (
+            <ReduxWrapper />
+        );
+    }
+}
 
 class Index extends React.Component<{}, {}>{
 
@@ -30,17 +132,19 @@ class Index extends React.Component<{}, {}>{
                 <div>
 
                     <ul>
-                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/home">Home</Link></li>
                         <li><Link to="/one">One</Link></li>
                         <li><Link to="/one/1">One dot 1</Link></li>
                         <li><Link to="/two">Two</Link></li>
+                        <li><Link to="/reduxyo">Redux</Link></li>
                     </ul>
 
                     <hr />
 
-                    <MyRoute path="/" component={ Home } />
+                    <MyRoute path="/home" component={ Home } />
                     <MyRoute path="/one/:id?" component={ One } />
-                    <MyRoute path="/two" component={ Two } />
+                    <MyRoute path="/two" component={Two} />
+                    <MyRoute path="/reduxyo" component={ReduxComponent } />
 
                 </div>
 
