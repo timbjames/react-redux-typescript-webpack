e61226ed3d3b024e95844566d3e352ce73b99919
Tim James <tim@kingfisher-systems.co.uk>
2017-07-27 09:32:46 +0100

    missing one file

--- SimpleTSTest/Scripts/App/Views/Home/One.tsx
+++ SimpleTSTest/Scripts/App/Views/Home/One.tsx
@@ -1,6 +1,9 @@
 ﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
-export const One = () => {
+interface IRouteParams extends RouteComponentProps<{ id: number }> { }
+
+export const One = (props: IRouteParams) => {
 
     return (
         <div><h1>Page One</h1></div>
