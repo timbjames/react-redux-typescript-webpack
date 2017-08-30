f430052af7abb972d066c5f2ea5bb6d451c26f1b
Tim James <tim@kingfisher-systems.co.uk>
2017-08-30 13:31:30 +0100

    ignoring wwwroot scripts

--- CoreWebApplication/wwwroot/scripts/App/Views/Home/Index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-exports.Index = function () {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "Index Page")));
-};
-//# sourceMappingURL=Index.js.map
--- CoreWebApplication/wwwroot/scripts/App/Views/Home/Two.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-exports.Two = function () {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "Page Two")));
-};
-//# sourceMappingURL=Two.js.map
